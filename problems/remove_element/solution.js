a322f309baecda038acc30ec08957cf8bbb343db
Arsenii <119811150+ArseniiMakhov@users.noreply.github.com>
2023-07-25 17:22:49 +0000

    Sync LeetCode submission - Remove Element (javascript)

---
+++ problems/remove_element/solution.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    let k = 0;
+    for (let n of nums) {
+        if(n !== val) {
+            nums[k++] = n;
+        }
+    }
+    return k
+};
